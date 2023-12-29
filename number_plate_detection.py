8e178bf1bfe3678c4ec07d9838039538781dfe45
RidwanulHaque111 <1705111@ugrad.cse.buet.ac.bd>
2023-12-29 19:27:35 +0600

    first commit

---
+++ number_plate_detection.py
@@ -0,0 +1,19 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import pytesseract as pt
+import plotly.express as px
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as xet
+
+from glob import glob
+from skimage import io
+from shutil import copy
+from tensorflow.keras.models import Model
+from tensorflow.keras.callbacks import TensorBoard
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.applications import InceptionResNetV2
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
